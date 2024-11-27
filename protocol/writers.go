bb2348e146cee75540abb1490c0fa07ca19d2ecf
Piyush Singariya <piyushsingariya@datazip.io>
2024-11-27 19:17:51 +0530

    altering Writers middleware
    Signed-off-by: Piyush Singariya <piyushsingariya@datazip.io>

--- protocol/writers.go
+++ protocol/writers.go
@@ -74,14 +74,16 @@ func (w *WriterPool) NewThread(parent context.Context, stream Stream) (InsertFun
 	}
 
 	w.threadCounter.Add(1)
-	frontend := make(chan types.Record) // To be given to Reader
-	backend := make(chan types.Record)  // To be given to Writer
+	frontend := make(chan types.Record)  // To be given to Reader
+	backend := make(chan []types.Record) // To be given to Writer
 	errChan := make(chan error)
 	child, childCancel := context.WithCancel(parent)
 
 	// spawnWriter spawns a writer process with child context
 	spawnWriter := func() {
 		w.group.Go(func() error {
+			defer childCancel() // spawnWriter uses childCancel to exit the middleware
+
 			err := func() error {
 				defer w.threadCounter.Add(-1)
 
@@ -103,58 +105,81 @@ func (w *WriterPool) NewThread(parent context.Context, stream Stream) (InsertFun
 
 	// middleware that has abstracted the repetition code from Writers
 	w.group.Go(func() error {
-		defer safego.Close(backend)
-		defer safego.Close(frontend)
-		// not defering canceling the child context so that writing process
-		// can finish writing all the records pushed into the channel
-		flatten := thread.Flattener()
-	main:
-		for {
-			select {
-			case <-parent.Done():
-				break main
-			default:
-				record, ok := <-frontend
-				if !ok {
+		err := func() error {
+			defer safego.Close(backend)
+			defer func() {
+				safego.Close(frontend)
+			}()
+			// not defering canceling the child context so that writing process
+			// can finish writing all the records pushed into the channel
+			flatten := thread.Flattener()
+			records := []types.Record{}
+		main:
+			for {
+				select {
+				case <-child.Done():
 					break main
-				}
+				case <-parent.Done():
+					break main
+				default:
+					// Note: Why push logic is not at the end of the code block
+					// i.e. because if Writer has exited before pushing into the channel;
+					// first the code will be blocked, second we might endup printing wrong state
+					if w.TotalRecords()%int64(batchSize_) == 0 {
+						backend <- records
+						if !state.IsZero() {
+							logger.LogState(state)
+						}
 
-				record, err := flatten(record) // flatten the record first
-				if err != nil {
-					return err
-				}
-
-				change, typeChange, mutations := fields.Process(record)
-				if change || typeChange {
-					w.tmu.Lock()
-					stream.Schema().Override(fields.ToProperties()) // update the schema in Stream
-					w.tmu.Unlock()
-				}
-
-				// handle schema evolution here
-				if (typeChange && thread.ReInitiationOnTypeChange()) || (change && thread.ReInitiationOnNewColumns()) {
-					childCancel()                                   // Close the current writer and spawn new
-					child, childCancel = context.WithCancel(parent) // replace the original child context and cancel function
-					spawnWriter()                                   // spawn a writer with newer context
-				} else {
-					err := thread.EvolveSchema(mutations.ToProperties())
-					if err != nil {
-						return fmt.Errorf("failed to evolve schema: %s", err)
+						records = []types.Record{}
 					}
-				}
 
-				err = typeutils.ReformatRecord(fields, record)
-				if err != nil {
-					return err
-				}
+					record, ok := <-frontend
+					if !ok {
+						break main
+					}
 
-				w.recordCount.Add(1) // increase the record count
-				backend <- record
-				w.logState()
+					record, err := flatten(record) // flatten the record first
+					if err != nil {
+						return err
+					}
+
+					change, typeChange, mutations := fields.Process(record)
+					if change || typeChange {
+						w.tmu.Lock()
+						stream.Schema().Override(fields.ToProperties()) // update the schema in Stream
+						w.tmu.Unlock()
+					}
+
+					// handle schema evolution here
+					if (typeChange && thread.ReInitiationOnTypeChange()) || (change && thread.ReInitiationOnNewColumns()) {
+						childCancel()                                   // Close the current writer and spawn new
+						child, childCancel = context.WithCancel(parent) // replace the original child context and cancel function
+						spawnWriter()                                   // spawn a writer with newer context
+					} else if typeChange || change {
+						err := thread.EvolveSchema(mutations.ToProperties())
+						if err != nil {
+							return fmt.Errorf("failed to evolve schema: %s", err)
+						}
+					}
+
+					err = typeutils.ReformatRecord(fields, record)
+					if err != nil {
+						return err
+					}
+
+					w.recordCount.Add(1) // increase the record count
+					records = append(records, record)
+				}
 			}
+
+			return nil
+		}()
+		if err != nil {
+			errChan <- fmt.Errorf("error in writer middleware: %s", err)
 		}
 
-		return nil
+		return err
 	})
 
 	spawnWriter()
@@ -180,10 +205,4 @@ func (w *WriterPool) TotalRecords() int64 {
 
 func (w *WriterPool) Wait() error {
 	return w.group.Wait()
-}
-
-func (w *WriterPool) logState() {
-	if w.TotalRecords()%int64(batchSize_) == 0 && !state.IsZero() {
-		logger.LogState(state)
-	}
 }
