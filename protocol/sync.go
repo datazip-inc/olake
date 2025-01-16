6ab3399e2f6eb702bbee7c764f455c273a4bf24f
magicpotato007 <vharsha294@gmail.com>
2025-01-16 20:30:25 +0530

    added running running sync with statement

--- protocol/sync.go
+++ protocol/sync.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/datazip-inc/olake/logger"
-	"github.com/datazip-inc/olake/logger/console"
 	"github.com/datazip-inc/olake/types"
 	"github.com/datazip-inc/olake/utils"
 	"github.com/spf13/cobra"
@@ -54,18 +53,8 @@ var syncCmd = &cobra.Command{
 		}
 
 		state.Mutex = &sync.Mutex{}
-		message := types.Message{
-			Type:  types.StateMessage,
-			State: state,
-			Log: &types.Log{
-				Level:   console.INFO.String(),
-				Message: "Running sync with state:",
-			},
-		}
-		
-		if err := console.Print(console.INFO, message); err != nil {
-			return err
-		}
+		logger.Info("Running sync with state: ")
+		logger.LogState(state)
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, _ []string) error {
