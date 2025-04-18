fc66d2024897467c157c0411f2def8cc79aac412
Duke Dhal <duke@datazip.io>
2025-04-19 02:25:36 +0530

    datatype converter introduced, cdc iceberg fixed

--- pkg/waljs/filter.go
+++ pkg/waljs/filter.go
@@ -27,48 +27,113 @@ func NewChangeFilter(streams ...protocol.Stream) ChangeFilter {
 	return filter
 }
 
+// func (c ChangeFilter) FilterChange(lsn pglogrepl.LSN, change []byte, OnFiltered OnMessage) error {
+// 	var changes WALMessage
+// 	if err := json.NewDecoder(bytes.NewReader(change)).Decode(&changes); err != nil {
+// 		return fmt.Errorf("failed to parse change received from wal logs: %s", err)
+// 	}
+
+// 	if len(changes.Change) == 0 {
+// 		return nil
+// 	}
+
+// 	// TODO: Parallel process changes
+// 	for _, ch := range changes.Change {
+// 		stream, exists := c.tables[utils.StreamIdentifier(ch.Table, ch.Schema)]
+// 		if !exists {
+// 			continue
+// 		}
+
+// 		changesMap := map[string]any{}
+// 		if ch.Kind == "delete" {
+// 			for i, changedValue := range ch.Oldkeys.Keyvalues {
+// 				columnType := ch.Oldkeys.Keytypes[i]
+// 				// logger.Debugf("ColumnType: %s", columnType)
+// 				convertedValue, err := converter(changedValue, columnType)
+// 				if err != nil {
+// 					return err
+// 				}
+// 				// logger.Debugf("convertedValue has type %T (value=%v)", convertedValue, convertedValue)
+// 				changesMap[ch.Oldkeys.Keynames[i]] = convertedValue
+// 			}
+// 		} else {
+// 			for i, changedValue := range ch.Columnvalues {
+// 				columnType := ch.Columntypes[i]
+// 				// logger.Debugf("ColumnType: %s", columnType)
+// 				convertedValue, err := converter(changedValue, columnType)
+// 				if err != nil {
+// 					return err
+// 				}
+// 				// logger.Debugf("convertedValue has type %T (value=%v)", convertedValue, convertedValue)
+// 				changesMap[ch.Columnnames[i]] = convertedValue
+// 			}
+// 		}
+
+// 		err := OnFiltered(CDCChange{
+// 			Stream:    stream,
+// 			Kind:      ch.Kind,
+// 			Schema:    ch.Schema,
+// 			Table:     ch.Table,
+// 			Timestamp: changes.Timestamp,
+// 			LSN:       lsn,
+// 			Data:      changesMap,
+// 		})
+
+// 		if err != nil {
+// 			return fmt.Errorf("failed to write filtered changed: %s", err)
+// 		}
+// 	}
+
+// 	return nil
+// }
+
 func (c ChangeFilter) FilterChange(lsn pglogrepl.LSN, change []byte, OnFiltered OnMessage) error {
 	var changes WALMessage
 	if err := json.NewDecoder(bytes.NewReader(change)).Decode(&changes); err != nil {
 		return fmt.Errorf("failed to parse change received from wal logs: %s", err)
 	}
-
 	if len(changes.Change) == 0 {
 		return nil
 	}
 
-	// TODO: Parallel process changes
 	for _, ch := range changes.Change {
 		stream, exists := c.tables[utils.StreamIdentifier(ch.Table, ch.Schema)]
 		if !exists {
 			continue
 		}
 
-		changesMap := map[string]any{}
+		data := make(map[string]any)
 		if ch.Kind == "delete" {
-			for i, changedValue := range ch.Oldkeys.Keyvalues {
-				changesMap[ch.Oldkeys.Keynames[i]] = changedValue
+			for i, val := range ch.Oldkeys.Keyvalues {
+				colType := ch.Oldkeys.Keytypes[i]
+				conv, err := converter(val, colType)
+				if err != nil {
+					return err
+				}
+				data[ch.Oldkeys.Keynames[i]] = conv
 			}
 		} else {
-			for i, changedValue := range ch.Columnvalues {
-				changesMap[ch.Columnnames[i]] = changedValue
+			for i, val := range ch.Columnvalues {
+				colType := ch.Columntypes[i]
+				conv, err := converter(val, colType)
+				if err != nil {
+					return err
+				}
+				data[ch.Columnnames[i]] = conv
 			}
 		}
 
-		err := OnFiltered(CDCChange{
+		if err := OnFiltered(CDCChange{
 			Stream:    stream,
 			Kind:      ch.Kind,
 			Schema:    ch.Schema,
 			Table:     ch.Table,
 			Timestamp: changes.Timestamp,
 			LSN:       lsn,
-			Data:      changesMap,
-		})
-
-		if err != nil {
-			return fmt.Errorf("failed to write filtered changed: %s", err)
+			Data:      data,
+		}); err != nil {
+			return fmt.Errorf("failed to write filtered change: %s", err)
 		}
 	}
-
 	return nil
 }
--- utils/sql.go
+++ utils/sql.go
@@ -1,9 +1,16 @@
 package utils
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 func MapScan(rows *sql.Rows, dest map[string]any) error {
 	columns, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+
+	types, err := rows.ColumnTypes()
 	if err != nil {
 		return err
 	}
@@ -18,11 +25,13 @@ func MapScan(rows *sql.Rows, dest map[string]any) error {
 	}
 
 	for i, col := range columns {
-		dest[col] = *(scanValues[i].(*any)) // Dereference pointer before storing
-		switch v := dest[col].(type) {
-		case []byte:
-			dest[col] = string(v)
+		rawData := *(scanValues[i].(*any)) // Dereference pointer before storing
+		dbType := types[i].DatabaseTypeName()
+		conv, err := converter(rawData, dbType)
+		if err != nil {
+			return err
 		}
+		dest[col] = conv
 	}
 
 	return nil
