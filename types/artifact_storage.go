deb6a6b2ca185ac89c2b1cad8bd953b9ab951ae3
Schitiz Sharma <schitiz@datazip.io>
2025-05-12 06:57:47 +0000

    add artifact storage config type

---
+++ types/artifact_storage.go
@@ -0,0 +1,13 @@
+package types
+
+type ArtifactStorageConfig struct {
+	Bucket       string `json:"bucket"`
+	Region       string `json:"region"`
+	BasePath     string `json:"base_path"`
+	AccessKey    string `json:"access_key,omitempty"`
+	SecretKey    string `json:"secret_key,omitempty"`
+	SessionToken string `json:"session_token,omitempty"`
+	Endpoint     string `json:"endpoint,omitempty"`
+	UseSSL       bool   `json:"use_ssl,omitempty"`
+	PathStyle    bool   `json:"path_style,omitempty"`
+}
