c600c8c2d11bd384dbddaaf3f421e3f226879be4
Schitiz Sharma <schitiz@datazip.io>
2025-05-12 12:40:31 +0000

    make basePath optional

--- types/artifact_storage.go
+++ types/artifact_storage.go
@@ -8,7 +8,7 @@ import (
 type ArtifactStorageConfig struct {
 	Bucket         string `json:"bucket"`
 	Region         string `json:"region"`
-	BasePath       string `json:"base_path"`
+	BasePath       string `json:"base_path,omitempty"`
 	AccessKey      string `json:"access_key,omitempty"`
 	SecretKey      string `json:"secret_key,omitempty"`
 	SessionToken   string `json:"session_token,omitempty"`
