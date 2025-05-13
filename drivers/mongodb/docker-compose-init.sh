1c67ac29996802d66cb90d652b47fd0161a5ae52
Duke Dhal <duke@datazip.io>
2025-05-13 12:10:27 +0530

    feat: workflows changes

--- drivers/mongodb/docker-compose-init.sh
+++ drivers/mongodb/docker-compose-init.sh
@@ -4,4 +4,5 @@ function mongosh() {
 
 mongosh 'rs.initiate()'
 sleep 3
-mongosh 'cfg = rs.conf(); cfg.members[0].host="localhost:27017"; rs.reconfig(cfg);'
+# mongosh 'cfg = rs.conf(); cfg.members[0].host="localhost:27017"; rs.reconfig(cfg);'
+mongosh 'rs.initiate({_id:"rs0", members: [{_id:0, host:"db:27017"}]})'
