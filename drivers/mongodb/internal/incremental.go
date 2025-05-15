678b541315cc1247e8339ad8f5502b27df43e19d
Jougan-0 <prasantmishra2018@gmail.com>
2025-05-15 16:32:16 +0530

    feat: multithread in incremental

--- drivers/mongodb/internal/incremental.go
+++ drivers/mongodb/internal/incremental.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/datazip-inc/olake/constants"
+	"github.com/datazip-inc/olake/drivers/base"
 	"github.com/datazip-inc/olake/logger"
 	"github.com/datazip-inc/olake/protocol"
 	"github.com/datazip-inc/olake/types"
@@ -14,16 +15,26 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readconcern"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
-	bmkField = "cursor_field"
-	bmkVal   = "cursor_value"
+	bmkField        = "cursor_field"
+	bmkVal          = "cursor_value"
+	defaultBatch    = 5_000
+	defaultChkEvery = 50_000
 )
 
 func (m *Mongo) incrementalSync(stream protocol.Stream, pool *protocol.WriterPool) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	db, collName := stream.Namespace(), stream.Name()
-	coll := m.client.Database(db).Collection(collName)
+	coll := m.client.
+		Database(db, options.Database().SetReadConcern(readconcern.Majority())).
+		Collection(collName)
+
 	cstream := stream.Self()
 
 	cursorField := cstream.CursorField
@@ -43,86 +54,141 @@ func (m *Mongo) incrementalSync(stream protocol.Stream, pool *protocol.WriterPoo
 	if stateVal != nil {
 		bookmark = coerceBookmark(stateVal)
 	}
-
 	if stateField != cursorField || bookmark == nil {
-		logger.Infof("cursor changed (%v→%s) or no bookmark; running back-fill",
+		logger.Infof("cursor changed (%v→%s) or empty bookmark – switching to back-fill",
 			stateField, cursorField)
-
 		if err := m.backfill(stream, pool); err != nil {
 			return err
 		}
-		if err := snapMaxAsBookmark(coll, cursorField, cstream, m); err != nil {
-			return err
-		}
-		return nil
+		return snapMaxAsBookmark(coll, cursorField, cstream, m)
 	}
 
 	batch := m.config.BatchSize
 	if batch == 0 {
-		batch = 5_000
+		batch = defaultBatch
 	}
-
-	logger.Infof("incremental sync on %s.%s (cursor=%s, start>%v, batch=%d)",
+	logger.Infof("incremental sync on %s.%s (cursor=%s, start≥%v, batch=%d)",
 		db, collName, cursorField, bookmark, batch)
 
-	for {
-		cur, err := coll.Find(
-			context.TODO(),
-			bson.M{cursorField: bson.M{"$gt": bookmark}},
-			options.Find().
-				SetBatchSize(batch).
-				SetSort(bson.D{{Key: cursorField, Value: 1}}),
-		)
-		if err != nil {
-			return err
-		}
+	threads := m.config.MaxThreads
+	if threads == 0 {
+		threads = 1
+	}
 
-		thread, err := pool.NewThread(context.TODO(), stream)
-		if err != nil {
-			cur.Close(context.TODO())
-			return err
-		}
+	docsCh := make(chan bson.M, 32*threads)
+	g, ctx := errgroup.WithContext(ctx)
+	spawnWriters(g, ctx, threads, stream, pool, docsCh)
+	start := bookmark
+	lastSeen := start
 
-		var processed int
-		for cur.Next(context.TODO()) {
-			var doc bson.M
-			if err := cur.Decode(&doc); err != nil {
-				cur.Close(context.TODO())
+	g.Go(func() error {
+		opts := options.Find().
+			SetSort(bson.D{{Key: cursorField, Value: 1}}).
+			SetBatchSize(int32(batch)).
+			SetHint(bson.D{{Key: cursorField, Value: 1}})
+
+		var processed int64
+
+		return base.RetryOnBackoff(m.config.RetryCount, time.Minute, func() error {
+			filter := bson.M{cursorField: bson.M{"$gte": start}}
+
+			cur, err := coll.Find(ctx, filter, opts)
+			if err != nil {
+				return err
+			}
+			defer cur.Close(ctx)
+
+			lastSeen = start
+			for cur.Next(ctx) {
+				var doc bson.M
+				if err := cur.Decode(&doc); err != nil {
+					return err
+				}
+				rawCursor := doc[cursorField]
+				handleMongoObject(doc)
+
+				select {
+				case docsCh <- doc:
+				case <-ctx.Done():
+					return ctx.Err()
+				}
+
+				if rawCursor != nil {
+					lastSeen = coerceBookmark(rawCursor)
+				} else if v, ok := doc[normCursor]; ok {
+					lastSeen = coerceBookmark(v)
+				}
+
+				processed++
+				if processed%defaultChkEvery == 0 { // So the checkpointing is lightweight
+					m.State.SetCursor(cstream, bmkVal, lastSeen)
+					m.State.LogState()
+				}
+			}
+			if err := cur.Err(); err != nil {
 				return err
 			}
 
-			rawCursor := doc[cursorField]
-			handleMongoObject(doc)
+			close(docsCh)
 
-			hash := utils.GetKeysHash(doc, constants.MongoPrimaryID)
-			if err := thread.Insert(
-				types.CreateRawRecord(hash, doc, "r", time.Unix(0, 0)),
-			); err != nil {
-				cur.Close(context.TODO())
-				return err
-			}
-
-			var cv any
-			if rawCursor != nil {
-				cv = rawCursor
-			} else if v, ok := doc[normCursor]; ok {
-				cv = v
-			}
-			if cv != nil {
-				bookmark = coerceBookmark(cv)
-			}
-			processed++
-		}
-		cur.Close(context.TODO())
-		thread.Close()
-
-		if processed == 0 {
+			logger.Infof("incremental bookmark advanced to %v on field %s", lastSeen, cursorField)
 			return nil
-		}
+		})
+	})
 
-		m.State.SetCursor(cstream, bmkField, cursorField)
-		m.State.SetCursor(cstream, bmkVal, bookmark)
-		m.State.LogState()
+	if err := g.Wait(); err != nil {
+		cancel()
+		return err
+	}
+	m.State.SetCursor(cstream, bmkField, cursorField)
+	m.State.SetCursor(cstream, bmkVal, lastSeen)
+	m.State.LogState()
+	return nil
+}
+
+func spawnWriters(
+	g *errgroup.Group,
+	ctx context.Context,
+	n int,
+	stream protocol.Stream,
+	pool *protocol.WriterPool,
+	in <-chan bson.M,
+) {
+	if n <= 0 {
+		n = 1
+	}
+	for i := 0; i < n; i++ {
+		g.Go(func() error { return writeWorker(ctx, stream, pool, in) })
+	}
+}
+
+func writeWorker(
+	ctx context.Context,
+	stream protocol.Stream,
+	pool *protocol.WriterPool,
+	in <-chan bson.M,
+) error {
+	writer, err := pool.NewThread(ctx, stream, protocol.WithBackfill(false))
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	for {
+		select {
+		case doc, ok := <-in:
+			if !ok {
+				return nil
+			}
+			hash := utils.GetKeysHash(doc, constants.MongoPrimaryID)
+			if e := writer.Insert(
+				types.CreateRawRecord(hash, doc, "r", time.Unix(0, 0)),
+			); e != nil {
+				return e
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
 	}
 }
 
@@ -148,10 +214,11 @@ func snapMaxAsBookmark(
 	m *Mongo,
 ) error {
 	var doc bson.M
-	if err := coll.
-		FindOne(context.TODO(), bson.D{},
-			options.FindOne().SetSort(bson.D{{Key: field, Value: -1}})).
-		Decode(&doc); err != nil {
+	if err := coll.FindOne(
+		context.TODO(),
+		bson.D{},
+		options.FindOne().SetSort(bson.D{{Key: field, Value: -1}}),
+	).Decode(&doc); err != nil {
 		return err
 	}
 	if v, ok := doc[field]; ok {
