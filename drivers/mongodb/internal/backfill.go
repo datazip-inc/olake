2877a70ee13d53951be34fab83e507d15204078c
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2025-05-28 13:01:48 +0530

    removed extra curly brace at line 271 (mongodb/backfill.go)

--- drivers/mongodb/internal/backfill.go
+++ drivers/mongodb/internal/backfill.go
@@ -268,7 +268,7 @@ func (m *Mongo) splitChunks(ctx context.Context, collection *mongo.Collection, s
 		return chunks, nil
 	}
 
-	switch m.config.ChunkingStrategy { {
+	switch m.config.ChunkingStrategy {
 	case "timestamp":
 		return timestampStrategy()
 	default:
