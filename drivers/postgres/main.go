4abc3ecb26b5386cdbdccd92db48f4f939ee87ab
Tanisha <tanisha@datazip.io>
2025-06-18 19:09:05 +0530

    reverted test changes

--- drivers/postgres/main.go
+++ drivers/postgres/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/datazip-inc/olake"
 	driver "github.com/datazip-inc/olake/drivers/postgres/internal"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 func main() {
-	fmt.Println("go update test check")
 	driver := &driver.Postgres{
 		CDCSupport: false,
 	}
