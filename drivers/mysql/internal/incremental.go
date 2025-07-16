ed2d2d528aa5c83ddcef5eaa274f5ae102b7706c
Tanisha <tanisha@datazip.io>
2025-07-16 19:21:02 +0530

    resolved comments

--- drivers/mysql/internal/incremental.go
+++ drivers/mysql/internal/incremental.go
@@ -7,12 +7,15 @@ import (
 	"github.com/datazip-inc/olake/drivers/abstract"
 	"github.com/datazip-inc/olake/pkg/jdbc"
 	"github.com/datazip-inc/olake/types"
+	"github.com/datazip-inc/olake/utils/logger"
 )
 
 func (m *MySQL) StreamIncrementalChanges(ctx context.Context, stream types.StreamInterface, processFn abstract.BackfillMsgFn) error {
 	cursorField := stream.Cursor()
 	lastCursorValue := m.state.GetCursor(stream.Self(), cursorField)
-	query := fmt.Sprintf(`SELECT * FROM %s WHERE %s >= ? ORDER BY %s`, stream.Name(), cursorField, cursorField)
+	query := fmt.Sprintf(`SELECT * FROM %s.%s WHERE %s >= ? ORDER BY %s`, stream.Namespace(), stream.Name(), cursorField, cursorField)
+
+	logger.Infof("Starting incremental sync for stream[%s]", stream.ID())
 
 	rows, err := m.client.QueryContext(ctx, query, []any{lastCursorValue}...)
 	if err != nil {
