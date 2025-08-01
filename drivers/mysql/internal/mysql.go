d68cf6f6b9d379fb752ccfd4a1f51d5bc8e043f5
Tanisha <tanisha@datazip.io>
2025-08-01 16:39:53 +0530

    stream correction

--- drivers/mysql/internal/mysql.go
+++ drivers/mysql/internal/mysql.go
@@ -157,9 +157,7 @@ func (m *MySQL) ProduceSchema(ctx context.Context, streamName string) (*types.St
 			if err := rows.Scan(&columnName, &columnType, &dataType, &isNullable, &columnKey); err != nil {
 				return nil, fmt.Errorf("failed to scan column: %s", err)
 			}
-			stream.WithCursorField(columnName)
 			datatype := types.Unknown
-
 			if val, found := mysqlTypeToDataTypes[dataType]; found {
 				datatype = val
 			} else {
@@ -179,8 +177,7 @@ func (m *MySQL) ProduceSchema(ctx context.Context, streamName string) (*types.St
 	if err != nil && ctx.Err() == nil {
 		return nil, fmt.Errorf("failed to process table[%s]: %s", streamName, err)
 	}
-	
-	// Add all discovered fields as potential cursor fields
+
 	stream.Schema.Properties.Range(func(key, value interface{}) bool {
 		if fieldName, ok := key.(string); ok {
 			stream.WithCursorField(fieldName)
