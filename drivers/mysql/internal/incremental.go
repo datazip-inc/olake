3673e6615e27875b26c328e2a232379eea3771ce
Tanisha <tanisha@datazip.io>
2025-08-06 15:57:41 +0530

    merged query args

--- drivers/mysql/internal/incremental.go
+++ drivers/mysql/internal/incremental.go
@@ -13,39 +13,22 @@ import (
 )
 
 func (m *MySQL) StreamIncrementalChanges(ctx context.Context, stream types.StreamInterface, processFn abstract.BackfillMsgFn) error {
-	primaryCursor, secondaryCursor := stream.Cursor()
-	lastPrimaryCursorValue := m.state.GetCursor(stream.Self(), primaryCursor)
-	lastSecondaryCursorValue := m.state.GetCursor(stream.Self(), secondaryCursor)
-
-	//TODO:
-	// 1. we need to ensure that state never has nil values for any cursor in all cases
-	if lastPrimaryCursorValue == nil {
-		logger.Warnf("last primary cursor value is nil for stream[%s]", stream.ID())
-	}
-	if secondaryCursor != "" &&  lastSecondaryCursorValue == nil {
-		logger.Warnf("last secondary cursor value is nil for stream[%s]", stream.ID())
-	}
-
 	filter, err := jdbc.SQLFilter(stream, m.Type())
 	if err != nil {
 		return fmt.Errorf("failed to parse filter during chunk iteration: %s", err)
 	}
 
-	incrementalCondition, queryArgs, err := m.buildIncrementalCondition(primaryCursor, secondaryCursor, lastPrimaryCursorValue, lastSecondaryCursorValue)
+	incrementalCondition, queryArgs, err := m.buildIncrementalCondition(stream)
 	if err != nil {
 		return fmt.Errorf("failed to format cursor condition: %s", err)
 	}
 	filter = utils.Ternary(filter != "", fmt.Sprintf("(%s) AND (%s)", filter, incrementalCondition), incrementalCondition).(string)
 	query := fmt.Sprintf("SELECT * FROM `%s`.`%s` WHERE %s", stream.Namespace(), stream.Name(), filter)
 
-	var rows *sql.Rows
-	if secondaryCursor != "" && lastSecondaryCursorValue != nil {
-		rows, err = m.client.QueryContext(ctx, query, lastPrimaryCursorValue, lastSecondaryCursorValue)
-	} else {
-		rows, err = m.client.QueryContext(ctx, query, lastPrimaryCursorValue)
-	}
-
 	logger.Infof("Starting incremental sync for stream[%s] with filter: %s", stream.ID(), logger.InterpolateQueryPlaceholders(query, queryArgs))
+
+	var rows *sql.Rows
+	rows, err = m.client.QueryContext(ctx, query, queryArgs...)
 	if err != nil {
 		return fmt.Errorf("failed to execute incremental query: %s", err)
 	}
@@ -67,7 +50,19 @@ func (m *MySQL) StreamIncrementalChanges(ctx context.Context, stream types.Strea
 }
 
 // buildIncrementalCondition generates the incremental condition SQL for MySQL
-func (m *MySQL) buildIncrementalCondition(primaryCursor string, secondaryCursor string, lastPrimaryCursorValue any, lastSecondaryCursorValue any) (string, []any, error) {
+func (m *MySQL) buildIncrementalCondition(stream types.StreamInterface) (string, []any, error) {
+	primaryCursor, secondaryCursor := stream.Cursor()
+	lastPrimaryCursorValue := m.state.GetCursor(stream.Self(), primaryCursor)
+	lastSecondaryCursorValue := m.state.GetCursor(stream.Self(), secondaryCursor)
+
+	//TODO:
+	// 1. we need to ensure that state never has nil values for any cursor in all cases
+	if lastPrimaryCursorValue == nil {
+		logger.Warnf("last primary cursor value is nil for stream[%s]", stream.ID())
+	}
+	if secondaryCursor != "" && lastSecondaryCursorValue == nil {
+		logger.Warnf("last secondary cursor value is nil for stream[%s]", stream.ID())
+	}
 	primaryCondition := fmt.Sprintf("`%s` >= ?", primaryCursor)
 	queryArgs := []any{lastPrimaryCursorValue}
 	if secondaryCursor != "" && lastSecondaryCursorValue != nil {
