9fb1b2695189db22498637fbd1ef3a06304e88a8
vishalm0509 <vishal@datazip.io>
2025-09-02 13:56:27 +0530

    fix: add type to jsonschema

--- constants/uischema.go
+++ constants/uischema.go
@@ -107,51 +107,67 @@ const OracleUISchema = `{
 }`
 
 const ParquetUISchema = `{
+  "type": {
+    "ui:widget": "hidden"
+  },
+  "writer": {
     "ui:grid": [
         { "s3_bucket": 12, "s3_region": 12 },
         { "s3_access_key": 12, "s3_secret_key": 12 },
         { "s3_path": 12 }
-    ]
+    ],
+    "ui:options": {
+      "label": false
+    }
+  }
 }`
 
 const IcebergUISchema = `{
-  "ui:grid" : [
-    {"catalog_type": 12},
-    {"rest_catalog_url": 12, "hive_uri": 12},
-    {"jdbc_url": 12, "jdbc_username" :12, "jdbc_password" :12},
-    {"iceberg_s3_path": 12, "iceberg_db": 12},
-    {"hive_clients": 12, "s3_use_ssl": 12},
-    {"hive_sasl_enabled": 12, "s3_path_style": 12},
-    {"grpc_port": 12, "sink_rpc_server_host": 12},
-    {"rest_auth_type": 12, "token": 12},
-    {"oauth2_uri": 12, "credential": 12},
-    {"no_identifier_fields" :12, "rest_signing_name" :12},
-    {"rest_signing_region" :12, "rest_signing_v_4" :12, "scope": 12},
-    {"s3_endpoint": 12, "aws_access_key": 12},
-    {"aws_secret_key": 12, "aws_region": 12}
-  ],
-  "no_identifier_fields": {
-    "ui:widget": "boolean"
+  "type": {
+    "ui:widget": "hidden"
   },
-  "rest_signing_v_4": {
-    "ui:widget": "boolean"
-  },
-  "s3_use_ssl": {
-    "ui:widget": "boolean"
-  },
-  "hive_sasl_enabled": {
-    "ui:widget": "boolean"
-  },
-  "s3_path_style": {
-    "ui:widget": "boolean"
-  },
-  "catalog_type": {
-    "ui:enumNames": [
-      "AWS Glue",
-      "JDBC",
-      "Hive",
-      "REST"
-    ]
+  "writer": {
+    "ui:grid": [
+      { "catalog_type": 12 },
+      { "rest_catalog_url": 12, "hive_uri": 12 },
+      { "jdbc_url": 12, "jdbc_username": 12, "jdbc_password": 12 },
+      { "iceberg_s3_path": 12, "iceberg_db": 12 },
+      { "hive_clients": 12, "s3_use_ssl": 12 },
+      { "hive_sasl_enabled": 12, "s3_path_style": 12 },
+      { "grpc_port": 12, "sink_rpc_server_host": 12 },
+      { "rest_auth_type": 12, "token": 12 },
+      { "oauth2_uri": 12, "credential": 12 },
+      { "no_identifier_fields": 12, "rest_signing_name": 12 },
+      { "rest_signing_region": 12, "rest_signing_v_4": 12, "scope": 12 },
+      { "s3_endpoint": 12, "aws_access_key": 12 },
+      { "aws_secret_key": 12, "aws_region": 12 }
+    ],
+    "no_identifier_fields": {
+      "ui:widget": "boolean"
+    },
+    "rest_signing_v_4": {
+      "ui:widget": "boolean"
+    },
+    "s3_use_ssl": {
+      "ui:widget": "boolean"
+    },
+    "hive_sasl_enabled": {
+      "ui:widget": "boolean"
+    },
+    "s3_path_style": {
+      "ui:widget": "boolean"
+    },
+    "catalog_type": {
+      "ui:enumNames": [
+        "AWS Glue",
+        "JDBC",
+        "Hive",
+        "REST"
+      ]
+    },
+    "ui:options": {
+      "label": false
+    }
   }
 }`
 
