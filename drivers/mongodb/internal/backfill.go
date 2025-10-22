7fb05af14c6faa48600f1a1adf413defe3d6f93b
vr-varad <varadgupta21@gmail.com>
2025-10-22 22:37:11 +0530

    Fix: take val as any type in mongodb

--- drivers/mongodb/internal/backfill.go
+++ drivers/mongodb/internal/backfill.go
@@ -398,22 +398,29 @@ func buildMongoCondition(cond types.Condition) bson.D {
 		"!=": "$ne",
 	}
 	//TODO: take val as any type
-	value := func(field, val string) interface{} {
+	value := func(field string, val any) interface{} {
 		// Handle unquoted null
-		if val == "null" {
+		switch v := val.(type) {
+		case nil:
 			return nil
-		}
-
-		if strings.HasPrefix(val, "\"") && strings.HasSuffix(val, "\"") {
-			val = val[1 : len(val)-1]
-		}
-		if field == "_id" && len(val) == 24 {
-			if oid, err := primitive.ObjectIDFromHex(val); err == nil {
-				return oid
+		case string:
+			if v == "null" {
+				return nil
 			}
-		}
-		if strings.ToLower(val) == "true" || strings.ToLower(val) == "false" {
-			return strings.ToLower(val) == "true"
+
+			if strings.HasPrefix(v, "\"") && strings.HasSuffix(v, "\"") {
+				v = v[1 : len(v)-1]
+			}
+			if field == "_id" && len(v) == 24 {
+				if oid, err := primitive.ObjectIDFromHex(v); err == nil {
+					return oid
+				}
+			}
+			if strings.ToLower(v) == "true" || strings.ToLower(v) == "false" {
+				return strings.ToLower(v) == "true"
+			}
+
+			val = v
 		}
 		if timeVal, err := typeutils.ReformatDate(val); err == nil {
 			return timeVal
