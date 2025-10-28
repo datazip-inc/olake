837c96534167e97c3ca5ea10734ea19d59383fdb
Duke Dhal <duke@datazip.io>
2025-10-28 13:16:08 +0530

    fix: changes

--- drivers/kafka/internal/incremental.go
+++ drivers/kafka/internal/incremental.go
@@ -2,15 +2,16 @@ package driver
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/datazip-inc/olake/drivers/abstract"
 	"github.com/datazip-inc/olake/types"
 )
 
 func (k *Kafka) StreamIncrementalChanges(_ context.Context, _ types.StreamInterface, _ abstract.BackfillMsgFn) error {
-	return nil
+	return fmt.Errorf("StreamIncrementalChanges not supported for Kafka driver")
 }
 
 func (k *Kafka) FetchMaxCursorValues(ctx context.Context, stream types.StreamInterface) (any, any, error) {
-	return nil, nil, nil
+	return nil, nil, fmt.Errorf("FetchMaxCursorValues not supported for Kafka driver")
 }
