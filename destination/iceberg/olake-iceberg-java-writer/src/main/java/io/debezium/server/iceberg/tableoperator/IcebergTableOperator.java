0df14efe4f6be8a9b8a15483f99eacdde76b9dc0
badalprasadsingh <badal@datazip.io>
2025-12-15 19:23:19 +0530

    fix: minor identations
    Signed-off-by: badalprasadsingh <badal@datazip.io>

--- destination/iceberg/olake-iceberg-java-writer/src/main/java/io/debezium/server/iceberg/tableoperator/IcebergTableOperator.java
+++ destination/iceberg/olake-iceberg-java-writer/src/main/java/io/debezium/server/iceberg/tableoperator/IcebergTableOperator.java
@@ -8,11 +8,9 @@
 
 package io.debezium.server.iceberg.tableoperator;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.google.common.collect.ImmutableMap;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
 import org.apache.iceberg.AppendFiles;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.DataFiles;
@@ -37,10 +35,10 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.ImmutableMap;
-
-import jakarta.enterprise.context.Dependent;
-import jakarta.inject.Inject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 /**
  * Wrapper to perform operations on iceberg tables
  *
