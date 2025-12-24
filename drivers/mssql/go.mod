feb0340eba55441bf05ab60a6c8e27692a2102b4
vishalm0509 <vishal@datazip.io>
2025-12-24 14:20:49 +0530

    chore: add integration test for mssql

--- drivers/mssql/go.mod
+++ drivers/mssql/go.mod
@@ -8,17 +8,20 @@ replace (
 )
 
 require (
+	github.com/apache/arrow-go/v18 v18.2.0
 	github.com/datazip-inc/olake v0.0.0-00010101000000-000000000000
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/microsoft/go-mssqldb v1.9.5
+	github.com/stretchr/testify v1.11.1
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.7.0 // indirect
 	dario.cat/mergo v1.0.1 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
-	github.com/apache/arrow-go/v18 v18.2.0 // indirect
+	github.com/apache/spark-connect-go/v35 v35.0.0-20250317154112-ffd832059443 // indirect
 	github.com/apache/thrift v0.21.0 // indirect
 	github.com/aws/aws-sdk-go v1.55.6 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.36.5 // indirect
@@ -46,10 +49,12 @@ require (
 	github.com/docker/docker v28.3.3+incompatible // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/ebitengine/purego v0.8.2 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-errors/errors v1.5.1 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
@@ -68,10 +73,12 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/asmfmt v1.3.2 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.10 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
@@ -79,6 +86,9 @@ require (
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
 	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/minio-go/v7 v7.0.34 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/hashstructure v1.1.0 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/go-archive v0.1.0 // indirect
@@ -87,6 +97,8 @@ require (
 	github.com/moby/sys/user v0.4.0 // indirect
 	github.com/moby/sys/userns v0.1.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
@@ -100,6 +112,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/rs/xid v1.6.0 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/sagikazarmark/locafero v0.8.0 // indirect
 	github.com/shirou/gopsutil/v4 v4.25.1 // indirect
@@ -111,7 +124,6 @@ require (
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/spf13/viper v1.20.1 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/testcontainers/testcontainers-go v0.37.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
@@ -132,6 +144,7 @@ require (
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
 	golang.org/x/mod v0.25.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
@@ -140,6 +153,7 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250728155136-f173205681a0 // indirect
 	google.golang.org/grpc v1.74.2 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
+	gopkg.in/ini.v1 v1.66.6 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ drivers/mssql/internal/mssql_test_util.go
@@ -0,0 +1,401 @@
+package driver
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/apache/arrow-go/v18/arrow"
+	"github.com/datazip-inc/olake/utils"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/microsoft/go-mssqldb"
+	"github.com/stretchr/testify/require"
+)
+
+func ExecuteQuery(ctx context.Context, t *testing.T, streams []string, operation string, fileConfig bool) {
+	t.Helper()
+
+	var connStr string
+	if fileConfig {
+		var config Config
+		utils.UnmarshalFile("./testdata/source.json", &config, false)
+
+		config.Host = fmt.Sprintf("%s:%d", config.Host, config.Port)
+
+		connStr = fmt.Sprintf("sqlserver://%s:%s@%s?database=%s&encrypt=disable",
+			config.Username,
+			config.Password,
+			config.Host,
+			config.Database,
+		)
+	} else {
+		connStr = "sqlserver://sa:Password!123@localhost:1433?database=olake_mssql_test&encrypt=disable"
+	}
+
+	db, err := sqlx.ConnectContext(ctx, "sqlserver", connStr)
+	require.NoError(t, err, "failed to connect to mssql")
+	defer func() {
+		require.NoError(t, db.Close())
+	}()
+
+	// integration test uses only one stream for testing
+	integrationTestTable := streams[0]
+
+	switch operation {
+	case "create":
+		// create the database if it doesn't exist
+		_, err := db.ExecContext(ctx, `IF DB_ID('olake_mssql_test') IS NULL BEGIN CREATE DATABASE olake_mssql_test; END;`)
+		require.NoError(t, err, "failed to ensure olake_mssql_test database exists")
+
+		_, err = db.ExecContext(ctx, `USE olake_mssql_test;`)
+		require.NoError(t, err, "failed to USE olake_mssql_test")
+
+		// enable CDC at DB level
+		_, err = db.ExecContext(ctx, `
+			IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'olake_mssql_test' AND is_cdc_enabled = 0)
+			BEGIN
+				EXEC sys.sp_cdc_enable_db;
+			END;
+		`)
+		require.NoError(t, err, "failed to enable CDC at DB level")
+
+		// Create the table in dbo schema
+		createTable := fmt.Sprintf(`
+			IF OBJECT_ID('dbo.%s', 'U') IS NULL
+			BEGIN
+				CREATE TABLE dbo.%s (
+					id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+					id_cursor INT NOT NULL,
+
+					col_tinyint TINYINT NOT NULL,
+					col_smallint SMALLINT NOT NULL,
+					col_int INT NOT NULL,
+					col_bigint BIGINT NOT NULL,
+					col_decimal DECIMAL(18,2) NOT NULL,
+					col_numeric NUMERIC(10,5) NOT NULL,
+					col_smallmoney SMALLMONEY NOT NULL,
+					col_money MONEY NOT NULL,
+					col_float FLOAT NOT NULL,
+					col_real REAL NOT NULL,
+					col_bit BIT NOT NULL,
+
+					col_char CHAR(10) NOT NULL,
+					col_varchar VARCHAR(255) NOT NULL,
+					col_text TEXT NOT NULL,
+					col_nchar NCHAR(10) NOT NULL,
+					col_nvarchar NVARCHAR(255) NOT NULL,
+					col_ntext NTEXT NOT NULL,
+
+					col_binary BINARY(16) NOT NULL,
+					col_varbinary VARBINARY(MAX) NOT NULL,
+
+					col_date DATE NOT NULL,
+					col_smalldatetime SMALLDATETIME NOT NULL,
+					col_datetime DATETIME NOT NULL,
+					col_datetime2 DATETIME2(6) NOT NULL,
+					col_datetimeoffset DATETIMEOFFSET(6) NOT NULL,
+					col_time TIME(0) NOT NULL,
+					col_uniqueidentifier UNIQUEIDENTIFIER NOT NULL,
+
+					col_int_nullable INT NULL,
+					col_varchar_nullable VARCHAR(255) NULL,
+					col_datetime2_nullable DATETIME2(6) NULL,
+
+					created_at DATETIME2(6) NOT NULL
+				);
+			END;
+		`, integrationTestTable, integrationTestTable)
+		_, err = db.ExecContext(ctx, createTable)
+		require.NoError(t, err, "failed to create integration test table")
+
+		// Enable CDC for table
+		enableTableCDC := fmt.Sprintf(`
+			IF NOT EXISTS (
+				SELECT 1 FROM cdc.change_tables WHERE source_object_id = OBJECT_ID('dbo.%s')
+			)
+			BEGIN
+				EXEC sys.sp_cdc_enable_table
+					@source_schema = N'dbo',
+					@source_name   = N'%s',
+					@role_name     = NULL,
+					@supports_net_changes = 0;
+			END;
+		`, integrationTestTable, integrationTestTable)
+		_, err = db.ExecContext(ctx, enableTableCDC)
+		require.NoError(t, err, "failed to enable CDC on integration test table")
+
+	case "drop":
+		_, err := db.ExecContext(ctx, fmt.Sprintf(`IF OBJECT_ID('dbo.%s','U') IS NOT NULL DROP TABLE dbo.%s;`, integrationTestTable, integrationTestTable))
+		require.NoError(t, err, "failed to drop integration test table")
+
+	case "clean":
+		_, err := db.ExecContext(ctx, fmt.Sprintf(`DELETE FROM dbo.%s;`, integrationTestTable))
+		require.NoError(t, err, "failed to clean integration test table")
+
+	case "add":
+		insertTestData(t, ctx, db, integrationTestTable)
+		return
+
+	case "insert":
+		insertOne := fmt.Sprintf(`
+			INSERT INTO dbo.%s (
+				id_cursor,
+				col_tinyint, col_smallint, col_int, col_bigint,
+				col_decimal, col_numeric, col_smallmoney, col_money,
+				col_float, col_real, col_bit,
+				col_char, col_varchar, col_text, col_nchar, col_nvarchar, col_ntext,
+				col_binary, col_varbinary,
+				col_date, col_smalldatetime, col_datetime, col_datetime2, col_datetimeoffset, col_time,
+				col_uniqueidentifier,
+				col_int_nullable, col_varchar_nullable, col_datetime2_nullable,
+				created_at
+			) VALUES (
+				6,
+				3, 5, 10, 19,
+				123.50, 10.12500, 1.2500, 2.5000,
+				123.50, 12.50, 1,
+				'char_val__', 'varchar_val', 'text_val', N'nchar_val_', N'nvarchar_val', N'ntext_val',
+				0x0102030405060708090A0B0C0D0E0F10, 0x01020304,
+				'2023-01-01', '2023-01-01 12:00:00', '2023-01-01 12:00:00',
+				'2023-01-01 12:00:00', '2023-01-01 12:00:00 +00:00', '13:14:15',
+				'123e4567-e89b-12d3-a456-426614174000',
+				NULL, NULL, NULL,
+				'2023-01-01 12:00:00'
+			);
+		`, integrationTestTable)
+		_, err := db.ExecContext(ctx, insertOne)
+		require.NoError(t, err, "failed to insert CDC row")
+
+	case "update":
+		updateRow := fmt.Sprintf(`
+			UPDATE dbo.%s SET
+				id_cursor = NULL,
+				col_bigint = 20,
+				col_decimal = 543.25,
+				col_money = 9.7500,
+				col_real = 321.0,
+				col_bit = 0,
+				col_varchar = 'updated varchar',
+				col_datetime2 = '2024-07-01 15:30:00',
+				col_datetimeoffset = '2024-07-01 15:30:00 +00:00',
+				col_varbinary = 0x0A0B0C,
+				col_uniqueidentifier = '00000000-0000-0000-0000-000000000000',
+				col_int_nullable = 123,
+				col_varchar_nullable = 'nullable updated',
+				col_datetime2_nullable = '2024-07-01 15:30:00',
+				created_at = '2024-07-01 15:30:00'
+			WHERE id = 1;
+		`, integrationTestTable)
+		_, err := db.ExecContext(ctx, updateRow)
+		require.NoError(t, err, "failed to update CDC row")
+
+	case "delete":
+		_, err := db.ExecContext(ctx, fmt.Sprintf(`DELETE FROM dbo.%s WHERE id = 1;`, integrationTestTable))
+		require.NoError(t, err, "failed to delete CDC row")
+
+	case "evolve-schema":
+		// Schema evolution: widen col_int from int -> bigint
+		stmt := fmt.Sprintf(`ALTER TABLE dbo.%s ALTER COLUMN col_int BIGINT NOT NULL;`, integrationTestTable)
+		_, err := db.ExecContext(ctx, stmt)
+		require.NoError(t, err, "failed to evolve schema")
+
+	default:
+		t.Fatalf("Unsupported operation: %s", operation)
+	}
+}
+
+func insertTestData(t *testing.T, ctx context.Context, db *sqlx.DB, tableName string) {
+	t.Helper()
+	for i := 1; i <= 5; i++ {
+		query := fmt.Sprintf(`
+			INSERT INTO dbo.%s (
+				id_cursor,
+				col_tinyint, col_smallint, col_int, col_bigint,
+				col_decimal, col_numeric, col_smallmoney, col_money,
+				col_float, col_real, col_bit,
+				col_char, col_varchar, col_text, col_nchar, col_nvarchar, col_ntext,
+				col_binary, col_varbinary,
+				col_date, col_smalldatetime, col_datetime, col_datetime2, col_datetimeoffset, col_time,
+				col_uniqueidentifier,
+				col_int_nullable, col_varchar_nullable, col_datetime2_nullable,
+				created_at
+			) VALUES (
+				%d,
+				3, 5, 10, 19,
+				123.50, 10.12500, 1.2500, 2.5000,
+				123.50, 12.50, 1,
+				'char_val__', 'varchar_val', 'text_val', N'nchar_val_', N'nvarchar_val', N'ntext_val',
+				0x0102030405060708090A0B0C0D0E0F10, 0x01020304,
+				'2023-01-01', '2023-01-01 12:00:00', '2023-01-01 12:00:00',
+				'2023-01-01 12:00:00', '2023-01-01 12:00:00 +00:00', '13:14:15',
+				'123e4567-e89b-12d3-a456-426614174000',
+				NULL, NULL, NULL,
+				'2023-01-01 12:00:00'
+			);
+		`, tableName, i)
+		_, err := db.ExecContext(ctx, query)
+		require.NoError(t, err, "Failed to insert test data row %d", i)
+	}
+}
+
+var ExpectedMSSQLData = map[string]interface{}{
+	// ints
+	"col_tinyint":  int32(3),
+	"col_smallint": int32(5),
+	"col_int":      int32(10),
+	"col_bigint":   int64(19),
+
+	// numerics
+	"col_decimal": float64(123.5),
+	"col_numeric": float64(10.125),
+	"col_float":   float64(123.5),
+	"col_real":    float32(12.5),
+
+	"col_bit": true,
+
+	// money
+	"col_smallmoney": "1.25",
+	"col_money":      "2.5",
+
+	// strings
+	"col_char":     "char_val__",
+	"col_varchar":  "varchar_val",
+	"col_text":     "text_val",
+	"col_nchar":    "nchar_val_",
+	"col_nvarchar": "nvarchar_val",
+	"col_ntext":    "ntext_val",
+
+	// binary
+	"col_binary":    "0102030405060708090a0b0c0d0e0f10",
+	"col_varbinary": "01020304",
+
+	// date/time
+	"col_date":           arrow.Timestamp(time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_smalldatetime":  arrow.Timestamp(time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_datetime":       arrow.Timestamp(time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_datetime2":      arrow.Timestamp(time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_datetimeoffset": arrow.Timestamp(time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"created_at":         arrow.Timestamp(time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_time":           "13:14:15",
+
+	"col_uniqueidentifier": "123e4567-e89b-12d3-a456-426614174000",
+}
+
+var ExpectedUpdatedMSSQLData = map[string]interface{}{
+	// ints
+	"col_tinyint":  int32(3),
+	"col_smallint": int32(5),
+	"col_int":      int32(10),
+	"col_bigint":   int64(20),
+
+	// numerics
+	"col_decimal": float64(543.25),
+	"col_numeric": float64(10.125),
+	"col_float":   float64(123.5),
+	"col_real":    float32(321.0),
+
+	// misc primitives
+	"col_bit": false,
+
+	// money
+	"col_smallmoney": "1.25",
+	"col_money":      "9.75",
+
+	// strings
+	"col_char":     "char_val__",
+	"col_varchar":  "updated varchar",
+	"col_text":     "text_val",
+	"col_nchar":    "nchar_val_",
+	"col_nvarchar": "nvarchar_val",
+	"col_ntext":    "ntext_val",
+
+	// binary -> hex strings
+	"col_binary":    "0102030405060708090a0b0c0d0e0f10",
+	"col_varbinary": "0a0b0c",
+
+	// date/time
+	"col_date":           arrow.Timestamp(time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_smalldatetime":  arrow.Timestamp(time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_datetime":       arrow.Timestamp(time.Date(2023, 1, 1, 12, 0, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_datetime2":      arrow.Timestamp(time.Date(2024, 7, 1, 15, 30, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_datetimeoffset": arrow.Timestamp(time.Date(2024, 7, 1, 15, 30, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+	"col_time":           "13:14:15",
+
+	"col_uniqueidentifier": "00000000-0000-0000-0000-000000000000",
+
+	"col_int_nullable":       int32(123),
+	"col_varchar_nullable":   "nullable updated",
+	"col_datetime2_nullable": arrow.Timestamp(time.Date(2024, 7, 1, 15, 30, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+
+	"created_at": arrow.Timestamp(time.Date(2024, 7, 1, 15, 30, 0, 0, time.UTC).UnixNano() / int64(time.Microsecond)),
+}
+
+// MSSQLToDestinationSchema
+var MSSQLToDestinationSchema = map[string]string{
+	"id":                     "int",
+	"col_tinyint":            "tinyint",
+	"col_smallint":           "smallint",
+	"col_int":                "int",
+	"col_bigint":             "bigint",
+	"col_decimal":            "double",
+	"col_numeric":            "double",
+	"col_smallmoney":         "double",
+	"col_money":              "double",
+	"col_float":              "double",
+	"col_real":               "real",
+	"col_bit":                "boolean",
+	"col_char":               "string",
+	"col_varchar":            "string",
+	"col_text":               "string",
+	"col_nchar":              "string",
+	"col_nvarchar":           "string",
+	"col_ntext":              "string",
+	"col_binary":             "string",
+	"col_varbinary":          "string",
+	"col_date":               "timestamp",
+	"col_smalldatetime":      "timestamp",
+	"col_datetime":           "timestamp",
+	"col_datetime2":          "timestamp",
+	"col_datetimeoffset":     "timestamp",
+	"col_time":               "string",
+	"col_uniqueidentifier":   "string",
+	"col_int_nullable":       "int",
+	"col_varchar_nullable":   "string",
+	"col_datetime2_nullable": "timestamp",
+	"created_at":             "timestamp",
+}
+
+var UpdatedMSSQLToDestinationSchema = map[string]string{
+	"id":                     "int",
+	"col_tinyint":            "tinyint",
+	"col_smallint":           "smallint",
+	"col_int":                "bigint", // evolved
+	"col_bigint":             "bigint",
+	"col_decimal":            "double",
+	"col_numeric":            "double",
+	"col_smallmoney":         "string",
+	"col_money":              "string",
+	"col_float":              "double",
+	"col_real":               "real",
+	"col_bit":                "boolean",
+	"col_char":               "string",
+	"col_varchar":            "string",
+	"col_text":               "string",
+	"col_nchar":              "string",
+	"col_nvarchar":           "string",
+	"col_ntext":              "string",
+	"col_binary":             "string",
+	"col_varbinary":          "string",
+	"col_date":               "timestamp",
+	"col_smalldatetime":      "timestamp",
+	"col_datetime":           "timestamp",
+	"col_datetime2":          "timestamp",
+	"col_datetimeoffset":     "timestamp",
+	"col_time":               "string",
+	"col_uniqueidentifier":   "string",
+	"col_int_nullable":       "int",
+	"col_varchar_nullable":   "string",
+	"col_datetime2_nullable": "timestamp",
+	"created_at":             "timestamp",
+}
