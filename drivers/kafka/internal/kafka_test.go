740e83dcf29165fa3a983d49ff6f0bb54d466672
Harshvir Potpose <hpotpose62@gmail.com>
2026-01-03 00:10:38 +0530

    add kafka integration tests
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

---
+++ drivers/kafka/internal/kafka_test.go
@@ -0,0 +1,25 @@
+package driver
+
+import (
+	"testing"
+
+	"github.com/datazip-inc/olake/constants"
+	"github.com/datazip-inc/olake/utils/testutils"
+)
+
+func TestKafkaIntegration(t *testing.T) {
+	t.Parallel()
+	testConfig := &testutils.IntegrationTest{
+		TestConfig:                       testutils.GetTestConfig(string(constants.Kafka)),
+		Namespace:                        "olake_kafka",
+		ExpectedData:                     ExpectedKafkaData,
+		ExpectedUpdatedData:              ExpectedUpdatedData,
+		DestinationDataTypeSchema:        KafkaToDestinationSchema,
+		UpdatedDestinationDataTypeSchema: KafkaToDestinationSchema,
+		ExecuteQuery:                     ExecuteQuery,
+		DestinationDB:                    "kafka_olake_kafka",
+		CursorField:                      "id_cursor:id_smallint",
+		PartitionRegex:                   "/{id,identity}",
+	}
+	testConfig.TestIntegration(t)
+}
