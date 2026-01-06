97de8824274035ce8f9c95668f54600af77962da
Duke Dhal <duke@datazip.io>
2026-01-06 15:46:33 +0530

    fix: changes

--- drivers/db2/docker-entrypoint-initdb.d/init.sql
+++
@@ -1,8 +0,0 @@
--- Connect to the test database
-CONNECT TO testdb;
-
--- Create the PUBLIC schema for testing
-CREATE SCHEMA PUBLIC AUTHORIZATION db2inst1;
-
--- Grant necessary permissions
-GRANT ALL ON SCHEMA PUBLIC TO db2inst1;
--- drivers/db2/internal/db2_test.go
+++ drivers/db2/internal/db2_test.go
@@ -11,13 +11,13 @@ func TestDB2Integration(t *testing.T) {
 	t.Parallel()
 	testConfig := &testutils.IntegrationTest{
 		TestConfig:                       testutils.GetTestConfig(string(constants.DB2)),
-		Namespace:                        "PUBLIC",
+		Namespace:                        "DB2INST1",
 		ExpectedData:                     ExpectedDB2Data,
 		ExpectedUpdatedData:              ExpectedUpdatedDB2Data,
 		DestinationDataTypeSchema:        DB2ToDestinationSchema,
 		UpdatedDestinationDataTypeSchema: UpdatedDB2ToDestinationSchema,
 		ExecuteQuery:                     ExecuteQuery,
-		DestinationDB:                    "db2_testdb_public",
+		DestinationDB:                    "db2_testdb_db2inst1",
 		CursorField:                      "COL_CURSOR:COL_SMALLINT",
 		PartitionRegex:                   "/{ID, identity}",
 	}
