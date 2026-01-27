8028b47c4bed617023cf5fa69048b163de062fb7
Tejas Tiwari <tt160705@outlook.com>
2026-01-27 20:39:02 +0530

    check - 2
    Signed-off-by: Tejas Tiwari <tt160705@outlook.com>

--- drivers/elasticsearch/internal/cdc.go
+++ drivers/elasticsearch/internal/cdc.go
@@ -14,11 +14,16 @@ func (e *Elasticsearch) PreCDC(_ context.Context, _ []types.StreamInterface) err
 }
 
 // StreamChanges streams CDC changes. CDC is not supported for Elasticsearch.
-func (e *Elasticsearch) StreamChanges(_ context.Context, _ types.StreamInterface, _ abstract.CDCMsgFn) error {
+func (e *Elasticsearch) StreamChanges(_ context.Context, _ int, _ abstract.CDCMsgFn) error {
 	return fmt.Errorf("CDC is not supported for Elasticsearch")
 }
 
 // PostCDC finalizes CDC state. CDC is not supported for Elasticsearch.
 func (e *Elasticsearch) PostCDC(_ context.Context, _ int) error {
 	return fmt.Errorf("CDC is not supported for Elasticsearch")
+}
+
+// ChangeStreamConfig configures change stream settings. Not applicable for Elasticsearch.
+func (e *Elasticsearch) ChangeStreamConfig() (bool, bool, bool) {
+	return false, false, false
 }
