adb66aac19bde2f90308e7331a45f3f4d95a2b03
vaibhav <vaibhav@datazip.io>
2026-02-19 18:13:06 +0530

    chore: made todo clearer

--- drivers/mongodb/internal/mon.go
+++ drivers/mongodb/internal/mon.go
@@ -168,7 +168,7 @@ func (m *Mongo) GetStreamNames(ctx context.Context) ([]string, error) {
 	return streamNames, collections.Err()
 }
 
-// TODO: remove sampling of table in produce schema when sync command is used
+// TODO: omit usage of ProduceSchema when sync command is used
 func (m *Mongo) ProduceSchema(ctx context.Context, streamName string) (*types.Stream, error) {
 	produceCollectionSchema := func(ctx context.Context, db *mongo.Database, streamName string) (*types.Stream, error) {
 		logger.Infof("producing type schema for stream [%s]", streamName)
