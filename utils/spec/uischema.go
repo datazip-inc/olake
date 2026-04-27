36bc349f648ee636b79490735ad2e97e85b3bbe0
vikash390 <vikash@datazip.io>
2026-04-27 14:33:48 +0530

    fix: schemas filed position in ui

--- utils/spec/uischema.go
+++ utils/spec/uischema.go
@@ -65,8 +65,7 @@ const PostgresUISchema = `{
         { "port": 12, "jdbc_url_params": 12 },
         { "ssl": 12, "max_threads": 12 },
         { "update_method": 12, "retry_count": 12 },
-        { "schemas": 24 },
-        { "ssh_config": 12 }
+        { "schemas": 12, "ssh_config": 12 }
       ],
       "schemas": {
         "ui:options": {
