be4cd2928c3af8eb3e805fcbb7bb647438f90d71
Saquib Ali <saquibali353@gmail.com>
2026-06-19 19:18:15 +0530

    fix: updated comment for cursorField selection

--- drivers/abstract/abstract.go
+++ drivers/abstract/abstract.go
@@ -113,8 +113,7 @@ func (a *AbstractDriver) Discover(ctx context.Context, maxDiscoverThreads int, i
 			convStream.SyncMode = types.CDC
 		} else if convStream.SupportedSyncModes.Exists(types.INCREMENTAL) {
 			convStream.SyncMode = types.INCREMENTAL
-			// If cursorField is already set, leave it unchanged.
-			// Otherwise, if availableCursorFields exist, cursorField defaults to first availableCursorField.
+			// If availableCursorFields exist, cursorField defaults to first availableCursorField.
 			if convStream.AvailableCursorFields.Len() > 0 {
 				convStream.CursorField = convStream.AvailableCursorFields.Array()[0]
 			}
