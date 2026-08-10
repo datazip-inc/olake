c4b95306726977a6a4fadccf03cafece623da7e8
Sahitya <sahitya@datazip.io>
2026-08-10 14:38:42 +0530

    chore: add S3 XML integration test variant

--- tests/s3/s3_test.go
+++ tests/s3/s3_test.go
@@ -10,6 +10,10 @@ import (
 // s3BaseConfig returns an IntegrationTest for one source format variant. Each variant owns a
 // testdata/<DataFormat>/ directory, which is what GetTestConfig's third argument selects.
 func s3BaseConfig(t *testing.T, variant S3TestVariant) *testutils.IntegrationTest {
+	filterConfig := S3FilterConfig
+	if variant.DataFormat == "xml" {
+		filterConfig = S3XMLFilterConfig
+	}
 	return &testutils.IntegrationTest{
 		TestConfig:                testutils.GetTestConfig(t, string(constants.S3), variant.DataFormat),
 		Namespace:                 "s3",
@@ -20,7 +24,7 @@ func s3BaseConfig(t *testing.T, variant S3TestVariant) *testutils.IntegrationTes
 		DestinationDB:             S3DestinationDB,
 		CursorField:               S3CursorField,
 		PartitionRegex:            S3PartitionRegex,
-		FilterConfig:              S3FilterConfig,
+		FilterConfig:              filterConfig,
 	}
 }
 
