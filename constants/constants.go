d54183c05a0a30964ae4bd7a91fc02888d794a55
saksham-datazip <saksham@datazip.io>
2026-09-01 16:38:09 +0530

    resolved-merge-conflict

--- constants/constants.go
+++ constants/constants.go
@@ -26,8 +26,8 @@ const (
 	StorageModeS3          = "s3"
 	EnvS3Region            = "OLAKE_S3_REGION"
 	EnvS3AccessKeyID       = "OLAKE_S3_ACCESS_KEY_ID"
-	EnvS3SecretAccessKey   = "OLAKE_S3_SECRET_ACCESS_KEY"
-	EnvS3SessionToken      = "OLAKE_S3_SESSION_TOKEN"
+	EnvS3SecretAccessKey   = "OLAKE_S3_SECRET_ACCESS_KEY" // #nosec G101 -- env var name, not a credential
+	EnvS3SessionToken      = "OLAKE_S3_SESSION_TOKEN"     // #nosec G101 -- env var name, not a credential
 	EnvS3Endpoint          = "OLAKE_S3_ENDPOINT"
 	ConfigFolder           = "CONFIG_FOLDER"
 	StatePath              = "STATE_PATH"
