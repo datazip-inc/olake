659bd8f6a23694cf1c0d7f0fd4570d57fd8c1231
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2026-09-03 00:38:41 +0530

    refactor: add descriptive comments and group CDC wait time constants

--- drivers/postgres/internal/postgres.go
+++ drivers/postgres/internal/postgres.go
@@ -25,7 +25,9 @@ import (
 )
 
 const (
-	minCDCInitialWaitTime     = 120
+	// minCDCInitialWaitTime is the minimum wait time in seconds for CDC synchronization.
+	minCDCInitialWaitTime = 120
+	// defaultCDCInitialWaitTime is the default wait time in seconds for CDC synchronization.
 	defaultCDCInitialWaitTime = 10800
 )
 
